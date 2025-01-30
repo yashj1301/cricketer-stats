167d63b764f0f6a6abc7bf1a49e47102c99a30d9
Yash Jain <yash.jain106@gmail.com>
2025-01-30 10:00:23 +0530

    first successful data scraping local test run.

--- test.py
+++ test.py
@@ -1,27 +1,31 @@
 from scripts.scraper.scraper import Cricketer_Stats_Scraper as PlayerStatsScraper
+import pandas as pd
 
 # Example usage:
-player_name = "Hardik Pandya"
+player_name = "Virat Kohli"
 player = PlayerStatsScraper(player_name)
 
 #Get player stats (batting, bowling, and check for allrounder stats)
 player.get_player_stats()
 
-# Print the player's batting stats
-print(player.battingstats)
-
-# Print the player's bowling stats
-print(player.bowlingstats)
-
-# Print the player's fielding stats
-print(player.fieldingstats)
-
 if player.allroundstats is not None:
-    print(player.allroundstats)
+    print(f"{player_name} is an all-rounder. Here are the stats:")
+    pd.to_csv(player.allroundstats, "/data/player_allrounder_stats.csv")
 else:
     print(f"{player_name} is not an all-rounder.")
 
-# Print the player's personal info
-print(player.player_info)
+
+# export the player's personal info
+player.player_info.to_csv("data/player_personal_info.csv")
+print(f"Player personal info exported to data/player_personal_info.csv")
+
+player.battingstats.to_csv("data/player_batting_stats.csv")
+print(f"Player batting stats exported to data/player_batting_stats.csv")
+
+player.bowlingstats.to_csv("data/player_bowling_stats.csv")
+print(f"Player bowling stats exported to data/player_bowling_stats.csv")
+
+player.fieldingstats.to_csv("data/player_fielding_stats.csv")
+print(f"Player fielding stats exported to data/player_fielding_stats.csv")
 
 
