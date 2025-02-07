031e54dac6b399cd96a6a8144726f4193b8c1cf8
Yash Jain <yash.jain106@gmail.com>
2025-02-07 16:51:41 +0530

    data export and file name changes for scraper.

--- test.py
+++ test.py
@@ -6,26 +6,5 @@ player_name = "Virat Kohli"
 player = PlayerStatsScraper(player_name)
 
 #Get player stats (batting, bowling, and check for allrounder stats)
-player.get_player_stats()
-
-if player.allroundstats is not None:
-    print(f"{player_name} is an all-rounder. Here are the stats:")
-    pd.to_csv(player.allroundstats, "/data/player_allrounder_stats.csv")
-else:
-    print(f"{player_name} is not an all-rounder.")
-
-
-# export the player's personal info
-player.player_info.to_csv("data/player_personal_info.csv", index=False)
-print(f"Player personal info exported to data/player_personal_info.csv")
-
-player.battingstats.to_csv("data/player_batting_stats.csv", index=False)
-print(f"Player batting stats exported to data/player_batting_stats.csv")
-
-player.bowlingstats.to_csv("data/player_bowling_stats.csv", index=False)
-print(f"Player bowling stats exported to data/player_bowling_stats.csv")
-
-player.fieldingstats.to_csv("data/player_fielding_stats.csv", index=False)
-print(f"Player fielding stats exported to data/player_fielding_stats.csv")
-
+player.get_player_stats("personal_info",export=False)
 
