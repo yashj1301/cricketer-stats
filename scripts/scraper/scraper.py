48e3b168bfc0b67a155f68a41c7865c62ecabfd1
Yash Jain <yash.jain106@gmail.com>
2025-04-16 16:53:04 +0530

    transitioned ETL from GCP to AWS for more flexibility.

--- scripts/scraper/scraper.py
+++ scripts/scraper/scraper.py
@@ -5,8 +5,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import undetected_chromedriver as uc
 
-class Cricketer_Stats_Scraper:
+class ScrapeData:
 
     def __init__(self, player_name):
         self.player_name = player_name
@@ -21,12 +22,14 @@ class Cricketer_Stats_Scraper:
         self.player_info = None
 
         # Set up the WebDriver and open the search URL
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")  # Run in headless mode
-        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-        
+        options = uc.ChromeOptions()
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+
         print("Setting up WebDriver...")
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        self.driver = uc.Chrome(options=options)        
 
         # Call get_player_url() to fetch the player's URL and ID when the object is initialized
         self.get_player_url()
@@ -35,25 +38,16 @@ class Cricketer_Stats_Scraper:
         start_time = time.time()
         print(f"Extracting {self.player_name}'s player URL and Player ID....")
         search_url = f"https://search.espncricinfo.com/ci/content/site/search.html?search={self.player_name.lower().replace(' ', '%20')};type=player"
-        
+        self.driver.get(search_url)
 
         try:
-
-            # Open the search URL
-            self.driver.get(search_url)
-
-            # Extract the player ID and URL from the search results
             player_link_element = self.driver.find_element(By.CSS_SELECTOR, "h3.name.link-cta a")
             self.player_url = player_link_element.get_attribute("href")
             self.player_id = self.player_url.split('-')[-1]
-            
-            # record the time taken
             print(f"Extraction Successful for {self.player_name}.")
             end_time = time.time()
             print(f"Time taken to extract URL: {end_time - start_time:.2f} seconds")
-        
         except Exception as e:
-            
             print(f"Error in extracting {self.player_name}'s url:", e)
             return None, None
 
@@ -95,16 +89,15 @@ class Cricketer_Stats_Scraper:
             print(f"Starting extraction of {self.player_name}'s personal info....")
             
             # Start by opening the player info URL
-            search_url = f"https://www.espncricinfo.com/cricketers/{self.player_name.replace(' ', '-').lower()}-{self.player_id}"
-            self.driver.get(search_url)
+            self.driver.get(self.player_url)
 
             # Step 1: Extract headers within the specified div tag
             headers = self.driver.find_elements(By.XPATH, "//div[@class='ds-grid lg:ds-grid-cols-3 ds-grid-cols-2 ds-gap-4 ds-mb-8']//p[@class='ds-text-tight-m ds-font-regular ds-uppercase ds-text-typo-mid3']")
-            header_names = [header.text for header in headers]
+            header_names = ['Player ID','Player URL']+[header.text for header in headers]
 
             # Step 2: Extract values within the specified div tag
             values = self.driver.find_elements(By.XPATH, "//div[@class='ds-grid lg:ds-grid-cols-3 ds-grid-cols-2 ds-gap-4 ds-mb-8']//span[@class='ds-text-title-s ds-font-bold ds-text-typo']")
-            value_texts = [value.text for value in values]
+            value_texts = [self.player_id,self.player_url]+[value.text for value in values]
 
             # Step 3: Create a DataFrame from the extracted data
             player_info = pd.DataFrame([value_texts], columns=header_names)
@@ -120,12 +113,12 @@ class Cricketer_Stats_Scraper:
 
     def get_player_stats(self, stats_type="all"):
         try:
-            # Ensure that player_id is available
-            if not self.player_id:
+            # Ensure that player ID or player URL is available
+            if not (self.player_id or self.player_url):
                 print("Player ID is not available. Run get_player_url() first.")
                 return
             
-            # Fetch personal information if 'personalinfo' is passed
+            # Fetch personal information if 'personal_info' is passed
             if stats_type == "personal_info":
                 self.player_info = self.extract_player_info()
             
@@ -145,11 +138,10 @@ class Cricketer_Stats_Scraper:
 
             # Fetch fielding stats if 'all' or 'fielding' is passed
             if stats_type == "all" or stats_type == "fielding":
-                self.fieldingstats = self.extract_inns_data('fielding')        
+                self.fieldingstats = self.extract_inns_data('fielding')
 
         except Exception as e:
             print(f"Error in extracting stats for {self.player_name}: ", e)
-
 
     def __del__(self):
         try:
@@ -157,4 +149,3 @@ class Cricketer_Stats_Scraper:
             print("WebDriver closed successfully.")
         except Exception as e:
             print("Error while closing the WebDriver:", e)
-
